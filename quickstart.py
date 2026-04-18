907aab97b4fc311a839f30d46fe0977cc8df5a01
leo <ljgulliver256@gmail.com>
2026-04-19 00:30:11 +0100

    break out client creation into its own func

--- quickstart.py
+++ quickstart.py
@@ -13,7 +13,6 @@ from googleapiclient.errors import HttpError
 from email.message import EmailMessage
 
 
-
 load_dotenv()
 
 CRED_PATH = os.getenv("GOOGLE_CREDENTIAL_PATH")
@@ -23,7 +22,14 @@ RECIPIENT = os.getenv("EMAIL_RECIPIENT")
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/gmail.send", "https://www.googleapis.com/auth/gmail.readonly", "https://www.googleapis.com/auth/gmail.compose"]
 
+
+
 def get_credentials():
+  '''Load pre-authorized user credentials from the environment.
+  TODO(developer) - See https://developers.google.com/identity
+  for guides on implementing OAuth2 for the application.
+  '''
+
   creds = None
   # The file GOOGLE_TOKEN_PATH stores the user's access and refresh tokens, and is
   # created automatically when the authorization flow completes for the first
@@ -44,25 +50,19 @@ def get_credentials():
       token.write(creds.to_json())
 
   return creds
-  
 
- 
-def gmail_send_message():
+
+def create_gmail_api_client():
+  creds = get_credentials()
+  return build("gmail", "v1", credentials=creds)
+
+def gmail_send_message(client):
   """Create and send an email message
   Print the returned  message id
   Returns: Message object, including message id
-
-  Load pre-authorized user credentials from the environment.
-  TODO(developer) - See https://developers.google.com/identity
-  for guides on implementing OAuth2 for the application.
   """
 
-  creds = get_credentials() 
-
   try:
-    # create gmail api client
-    service = build("gmail", "v1", credentials=creds)
-
     message = EmailMessage()
 
     message.set_content(f"test {random.randint(1, 10)}")
@@ -78,7 +78,7 @@ def gmail_send_message():
     # dynamic attribute generation therefore:
     # pylint: disable=E1101
     send_message = (
-        service.users()
+        client.users()
         .messages()
         .send(userId="me", body=create_message)
         .execute()
@@ -94,4 +94,6 @@ def gmail_send_message():
 
 
 if __name__ == "__main__":
-  gmail_send_message()
+  gmail_client = create_gmail_api_client()
+  test = gmail_send_message(client=gmail_client)
+  print(test)
